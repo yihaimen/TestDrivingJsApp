21054aa91f3f66aa59e7c5aadc5d6999a1c92252
yihaimen <yihaimen@outlook.com>
2021-03-31 21:48:01 +0800

    chore: 内联变量

--- src/tdd/performance.js
+++ src/tdd/performance.js
@@ -10,37 +10,16 @@ function statement(invoice, plays) {
     }).format;
 
   for (let perf of invoice.performances) {
-    const play = playFor(perf);
-    const thisAmount = amountFor(play, perf);
-    // let thisAmount = 0;
-
-    // switch (play.type) {
-    //   case "tragedy":
-    //     thisAmount = 40000;
-    //     if (perf.audience > 30) {
-    //       thisAmount += 1000 * (perf.audience - 30);
-    //     }
-    //     break;
-    //   case "comedy":
-    //     thisAmount = 30000;
-    //     if (perf.audience > 20) {
-    //       thisAmount += 10000 + 500 * (perf.audience - 20);
-    //     }
-    //     thisAmount += 300 * perf.audience;
-    //     break;
-    //   default:
-    //     throw new Error(`unknown type: ${play.type}`);
-    // }
-
+    const thisAmount = amountFor(playFor(perf), perf);
     // add volume credits
     volumeCredits += Math.max(perf.audience - 30, 0);
     // add extra credit for every ten comedy attendees
-    if ("comedy" === play.type) {
+    if ("comedy" === playFor(perf).type) {
       volumeCredits += Math.floor(perf.audience / 5);
     }
 
     // print line for this order
-    result += ` ${play.name}: ${format(thisAmount/100)} (${perf.audience} seats)\n`;
+    result += ` ${playFor(perf).name}: ${format(thisAmount/100)} (${perf.audience} seats)\n`;
     totalAmount += thisAmount;
   }
 
