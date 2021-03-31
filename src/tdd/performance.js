2ec5adb6d0015ad78d9cbf90ca1e40d3442aed60
yihaimen <yihaimen@outlook.com>
2021-03-31 21:45:01 +0800

    chore: 以查询取代临时变量

--- src/tdd/performance.js
+++ src/tdd/performance.js
@@ -9,31 +9,8 @@ function statement(invoice, plays) {
       minimumFractionDigits: 2
     }).format;
 
-  function amountFor(play, perf) {
-    let result = 0;
-    switch (play.type) {
-      case "tragedy":
-        result = 40000;
-        if (perf.audience > 30) {
-          result += 1000 * (perf.audience - 30);
-        }
-        break;
-      case "comedy":
-        result = 30000;
-        if (perf.audience > 20) {
-          result += 10000 + 500 * (perf.audience - 20);
-        }
-        result += 300 * perf.audience;
-        break;
-      default:
-        throw new Error(`unknown type: ${play.type}`);
-    }
-  
-    return result;
-  }
-
   for (let perf of invoice.performances) {
-    const play = plays[perf.playID];
+    const play = playFor(perf);
     const thisAmount = amountFor(play, perf);
     // let thisAmount = 0;
 
@@ -70,6 +47,33 @@ function statement(invoice, plays) {
   result += `Amount owed is ${format(totalAmount/100)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
   return volumeCredits;
+
+  function amountFor(play, aPerformance) {
+    let result = 0;
+    switch (play.type) {
+      case "tragedy":
+        result = 40000;
+        if (aPerformance.audience > 30) {
+          result += 1000 * (aPerformance.audience - 30);
+        }
+        break;
+      case "comedy":
+        result = 30000;
+        if (aPerformance.audience > 20) {
+          result += 10000 + 500 * (aPerformance.audience - 20);
+        }
+        result += 300 * aPerformance.audience;
+        break;
+      default:
+        throw new Error(`unknown type: ${play.type}`);
+    }
+  
+    return result;
+  }
+
+  function playFor(aPerformance) {
+    return plays[aPerformance.playID];
+  }
 }
 
 module.exports = statement;
