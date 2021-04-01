893dbfa98fcea8a1af17e5e9667216e9bed93d97
yihaimen <yihaimen@outlook.com>
2021-04-01 23:16:22 +0800

    chore: 提炼函数 totalAmount

--- src/tdd/performance.js
+++ src/tdd/performance.js
@@ -1,15 +1,20 @@
 function statement(invoice, plays) {
-  let totalAmount = 0;
   let result = `Statement for ${invoice.customer}\n`;
   for (let perf of invoice.performances) {
     // print line for this order
     result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
-    totalAmount += amountFor(perf);
   }
-  
-  result += `Amount owed is ${usd(totalAmount)}\n`;
+  result += `Amount owed is ${usd(appleSauce())}\n`;
   result += `You earned ${totalVolumeCredits()} credits\n`;
   return totalVolumeCredits();
+
+  function appleSauce() {
+    let result = 0;
+    for (let perf of invoice.performances) {
+      result += amountFor(perf);
+    }
+    return result;
+  }
 
   function totalVolumeCredits() {
     let result = 0;
