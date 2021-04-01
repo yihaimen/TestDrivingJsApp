da80ef7e3933e5176b0ddec0f08a508929d3df54
yihaimen <yihaimen@outlook.com>
2021-04-01 23:05:38 +0800

    chore: 以查询取代临时变量 totalVolumeCredits

--- src/tdd/performance.js
+++ src/tdd/performance.js
@@ -7,10 +7,9 @@ function statement(invoice, plays) {
     totalAmount += amountFor(perf);
   }
   
-  let volumeCredits = totalVolumeCredits();
   result += `Amount owed is ${usd(totalAmount)}\n`;
-  result += `You earned ${volumeCredits} credits\n`;
-  return volumeCredits;
+  result += `You earned ${totalVolumeCredits()} credits\n`;
+  return totalVolumeCredits();
 
   function totalVolumeCredits() {
     let result = 0;
