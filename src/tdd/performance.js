8d8fa86b87111290bd0a570e87a4f2bc00d384f6
yihaimen <yihaimen@outlook.com>
2021-04-01 22:52:12 +0800

    chore: 提炼volumeCreditsFor函数

--- src/tdd/performance.js
+++ src/tdd/performance.js
@@ -10,12 +10,7 @@ function statement(invoice, plays) {
     }).format;
 
   for (let perf of invoice.performances) {
-    // add volume credits
-    volumeCredits += Math.max(perf.audience - 30, 0);
-    // add extra credit for every ten comedy attendees
-    if ("comedy" === playFor(perf).type) {
-      volumeCredits += Math.floor(perf.audience / 5);
-    }
+    volumeCredits += volumeCreditsFor(perf);
 
     // print line for this order
     result += ` ${playFor(perf).name}: ${format(amountFor(perf)/100)} (${perf.audience} seats)\n`;
@@ -25,6 +20,15 @@ function statement(invoice, plays) {
   result += `Amount owed is ${format(totalAmount/100)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
   return volumeCredits;
+
+  function volumeCreditsFor(perf) {
+    let volumeCredits = 0;
+    volumeCredits += Math.max(perf.audience - 30, 0);
+    if ("comedy" === playFor(perf).type) {
+      volumeCredits += Math.floor(perf.audience / 5);
+    }
+    return volumeCredits;
+  }
 
   function amountFor(aPerformance) {
     let result = 0;
